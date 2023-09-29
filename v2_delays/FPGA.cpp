6fc072a15a4af550f8b7cdf3dda261a5fb32657f
Eduardo Izquierdo <edizquie@indiana.edu>
2023-09-28 21:05:15 -0400

    v3

---
+++ v2_delays/FPGA.cpp
@@ -0,0 +1,242 @@
+// ***********************************************************
+// A class for a minimal simulation of an FPGA
+// ************************************************************
+
+#include "FPGA.h"
+#include "random.h"
+#include <stdlib.h>
+
+// **************************** 
+// Constructors and Destructors
+// ****************************
+
+// The constructor
+
+FPGA::FPGA(int newsize)
+{
+	SetBlockSize(newsize);
+}
+
+// The destructor
+
+FPGA::~FPGA()
+{
+	SetBlockSize(0);
+}
+
+// *********
+// Utilities
+// *********
+
+// Resize a block.
+
+void FPGA::SetBlockSize(int newsize)
+{
+	size = newsize;
+	states.SetBounds(1,size,1,size);
+	states.FillContents(0);
+
+	maxdelay = 3;
+	history.SetBounds(1,size*size,1,maxdelay);
+	history.FillContents(0);
+
+	// delays.SetBounds(1,size,1,size);
+	// delays.FillContents(1);
+	
+	gates.SetBounds(1,size,1,size);
+	gates.FillContents(0);
+	
+	Ax.SetBounds(1,size,1,size);
+	Ax.FillContents(0);
+	
+	Ay.SetBounds(1,size,1,size);
+	Ay.FillContents(0);
+	
+	Bx.SetBounds(1,size,1,size);
+	Bx.FillContents(0);
+	
+	By.SetBounds(1,size,1,size);
+	By.FillContents(0);
+}
+
+// *******
+// Control
+// *******
+
+// Initialize the states or outputs of a circuit.
+void FPGA::ZeroBlockState()
+{
+	for (int i = 1; i <= size; i++){
+		for (int j = 1; j <= size; j++){
+        	SetBlockState(i, j, 0);
+		}
+	}
+}
+
+void FPGA::OneBlockState()
+{
+	for (int i = 1; i <= size; i++){
+		for (int j = 1; j <= size; j++){
+        	SetBlockState(i, j, 1);
+		}
+	}
+}
+
+void FPGA::RandomizeBlockState()
+{
+	for (int i = 1; i <= size; i++){
+		for (int j = 1; j <= size; j++){
+			//cout << i << " " << j << " " << ProbabilisticChoice(0.5) << endl;
+        	SetBlockState(i, j, ProbabilisticChoice(0.5));
+		}
+	}
+}
+
+void FPGA::RandomizeBlockState(RandomState &rs)
+{
+	for (int i = 1; i <= size; i++){
+		for (int j = 1; j <= size; j++){
+        	SetBlockState(i, j, rs.ProbabilisticChoice(0.5));
+		}
+	}
+}
+
+void FPGA::UpdateHistory()
+{
+	// First, push each item back one time step
+	for (int t = 1; t < maxdelay; t++){
+		for (int i = 1; i <= size; i++) {
+			for (int j = 1; j <= size; j++) {
+				history[i+size*(j-1)][t+1] = history[i+size*j][t];
+			}
+		}
+	}
+	// Make the most recent one the current one
+	for (int i = 1; i <= size; i++) {
+		for (int j = 1; j <= size; j++) {
+			history[i+size*(j-1)][1] = states[i][j];
+		}
+	}
+}
+
+// Integrate the FPGA one step 
+
+void FPGA::Step()
+{
+	int A, B, x, y, delay;
+  // Update the state of all blocks and write into a temporary state
+  for (int i = 1; i <= size; i++) {
+	for (int j = 1; j <= size; j++) {
+		// Get A and B inputs
+		x = Ax[i][j];
+		y = Ay[i][j];
+		if ((x>0) && (x<=size) && (y>0) && (y<=size)){
+			// Make sure they are referring to an actual block, otherwise input set to 0
+			// Set it according to their respective delay
+			if (gates[x][y] == 0){ 
+				delay = 2;
+			}
+			else{
+				delay = 3;
+			}
+			A = history[x+size*(y-1)][delay];
+		}
+		else{
+			A = 0;
+		}
+		x = Bx[i][j];
+		y = By[i][j];		
+		if ((x>0) && (x<=size) && (y>0) && (y<=size)){
+			// Make sure they are referring to an actual block, otherwise input set to 0
+			if (gates[x][y] == 0){ 
+				delay = 2;
+			}
+			else{
+				delay = 3;
+			}
+			B = history[x+size*(y-1)][delay];
+
+		}
+		else{
+			B = 0;
+		}
+		// NAND
+		if (gates[i][j] == 0){ 
+			if ((A == 1) && (B == 1))
+				states[i][j] = 0;
+			else
+			 	states[i][j] = 1;
+		}
+		// NOR
+		else{
+			if ((A == 0) && (B == 0))
+				states[i][j] = 1;
+			else
+			 	states[i][j] = 0;
+		}
+	}
+  }
+}
+
+// ****************
+// Input and Output
+// ****************
+
+#include <iomanip>
+
+ostream& operator<<(ostream& os, FPGA& c)
+{
+	// Set the precision
+	os << setprecision(32);
+	// Write the size
+	os << c.size << endl << endl;
+	// Write the Gates
+	for (int i = 1; i <= c.size; i++) {
+		for (int j = 1; j <= c.size; j++)
+			os << c.gates[i][j] << " ";
+		os << endl;
+	}
+	os << endl << endl;
+	// Write the input A
+	for (int i = 1; i <= c.size; i++) {
+		for (int j = 1; j <= c.size; j++)
+			os << "(" << c.Ax[i][j] << "," << c.Ay[i][j] << ") ";
+		os << endl;
+	}
+	os << endl << endl;
+	// Write the input B
+	for (int i = 1; i <= c.size; i++) {
+		for (int j = 1; j <= c.size; j++)
+			os << "(" << c.Bx[i][j] << "," << c.By[i][j] << ") ";
+		os << endl;
+	}
+	os << endl << endl;
+	// Return the ostream
+	return os;
+}
+
+// istream& operator>>(istream& is, CTRNN& c)
+// {
+// 	// Read the size
+// 	int size;
+// 	is >> size;
+// 	c.SetCircuitSize(size);
+// 	// Read the time constants
+// 	for (int i = 1; i <= size; i++) {
+// 		is >> c.taus[i];
+// 		c.Rtaus[i] = 1/c.taus[i];
+// 	}
+// 	// Read the biases
+// 	for (int i = 1; i <= size; i++)
+// 		is >> c.biases[i];
+// 	// Read the gains
+// 	for (int i = 1; i <= size; i++)
+// 		is >> c.gains[i];
+// 	// Read the weights
+// 	for (int i = 1; i <= size; i++)
+// 		for (int j = 1; j <= size; j++)
+// 			is >> c.weights[i][j];
+// 	// Return the istream		
+// 	return is;
+// }		
+		
---
+++ v2_delays/main.cpp
@@ -0,0 +1,542 @@
+#include "BSearch.h"
+#include "FPGA.h"
+#include "random.h"
+
+#define PRINTOFILE
+
+// Task params
+const double DUR = 10; 
+
+// EA params
+const int POPSIZE = 32; //96;
+const int GENS = 100; //100;
+const double MUTVAR = 0.05;			
+const double CROSSPROB = 0.0;
+const double EXPECTED = 1.1;
+const double ELITISM = 0.02;
+
+// FPGA params
+const int N = 3; 
+
+int	VectSize = 5*N*N; // 2 bits for input A, 2 bits for input B, and 1 bit for Gate
+
+// ================================================
+// A. FUNCTIONS FOR EVOLVING A SUCCESFUL CIRCUIT
+// ================================================
+
+// ------------------------------------
+// Fitness function
+// ------------------------------------
+double FitnessFunction(TVector<int> &g, RandomState &rs)
+{
+	//cout << "\n" << g << endl;
+
+	// Create the board
+	FPGA board(N);
+
+	// Change the genotype to binary phenotype
+	// TVector<int> g;
+	// g.SetBounds(1,VectSize);
+	// for (int i = 1; i <= VectSize; i++){
+	// 	if (genotype[i] >= 0){
+	// 		g[i] = 1;
+	// 	}
+	// 	else{
+	// 		g[i] = 0;
+	// 	}
+	// }
+
+	// Set gates and inputs from genotype to board
+	int k = 1;
+	for (int i = 1; i <= N; i++){
+		for (int j = 1; j <= N; j++){
+			// Input A (does not check for in-bounds)
+			if ((g[k] == 0) && (g[k+1] == 0)){  // North
+				board.Ax[i][j] = i-1;
+				board.Ay[i][j] = j;
+			}
+			else{
+				if ((g[k] == 0) && (g[k+1] == 1)){  // East
+					board.Ax[i][j] = i;
+					board.Ay[i][j] = j+1;
+				}
+				else{
+					if ((g[k] == 1) && (g[k+1] == 0)){	// West
+						board.Ax[i][j] = i;
+						board.Ay[i][j] = j-1;
+					}
+					else{		// South
+							board.Ax[i][j] = i+1;
+							board.Ay[i][j] = j;						
+					}
+				}
+			}
+			k+=2;
+			// Input B (does not check for in-bounds)
+			if ((g[k] == 0) && (g[k+1] == 0)){  // North
+				board.Bx[i][j] = i-1;
+				board.By[i][j] = j;
+			}
+			else{
+				if ((g[k] == 0) && (g[k+1] == 1)){  // East
+					board.Bx[i][j] = i;
+					board.By[i][j] = j+1;
+				}
+				else{
+					if ((g[k] == 1) && (g[k+1] == 0)){	// West
+						board.Bx[i][j] = i;
+						board.By[i][j] = j-1;
+					}
+					else{		// South
+							board.Bx[i][j] = i+1;
+							board.By[i][j] = j;						
+					}
+				}
+			}
+			k+=2;
+			// Gates
+			board.gates[i][j] = g[k];
+			k++;
+		}
+	}
+
+	// Initialize state
+	board.ZeroBlockState();
+
+	//cout << board.gates << endl;
+	
+	// Count the number of times blocks change
+	double total = 0.0;
+	TMatrix<int> previousboardstate;
+	previousboardstate.SetBounds(1,N,1,N);
+	previousboardstate.FillContents(0.0);
+	// Save latest state
+	for (int i = 1; i <= N; i++){
+		for (int j = 1; j <= N; j++){
+			previousboardstate[i][j] = board.states[i][j];
+		}
+	}
+	// Simulate for DUR steps
+	for (int t = 1; t <= DUR; t++){
+		board.Step();
+		board.UpdateHistory();
+		double portion = 0.0;
+		// Analyze difference and count
+		for (int i = 1; i <= N; i++){
+			for (int j = 1; j <= N; j++){
+				if (board.states[i][j] != previousboardstate[i][j]){
+					portion += 1.0;
+				}
+			}
+		}
+		total += portion/(N*N);
+		// Save latest state
+		for (int i = 1; i <= N; i++){
+			for (int j = 1; j <= N; j++){
+				previousboardstate[i][j] = board.states[i][j];
+			}
+		}
+	}
+	//cout << total/DUR << endl;
+	return total/DUR;
+}
+
+// ------------------------------------
+// 2N Designed Behavior
+// ------------------------------------
+double DesignedBehavior()
+{
+	// Create the board
+	FPGA board(N);
+
+	TVector<int> g;
+	g.SetBounds(1,VectSize);
+
+	g[1]=0; g[2]=1; g[3]=1; g[4]=1; g[5]=1;
+	g[6]=1; g[7]=0; g[8]=1; g[9]=1; g[10]=0;
+	g[11]=0; g[12]=0; g[13]=0; g[14]=1; g[15]=0;
+	g[16]=1; g[17]=0; g[18]=0; g[19]=0; g[20]=1;
+	cout << g << endl;
+
+	// Set gates and inputs from genotype to board
+	int k = 1;
+	for (int i = 1; i <= N; i++){
+		for (int j = 1; j <= N; j++){
+			// Input A (does not check for in-bounds)
+			if ((g[k] == 0) && (g[k+1] == 0)){  // North
+				board.Ax[i][j] = i-1;
+				board.Ay[i][j] = j;
+			}
+			else{
+				if ((g[k] == 0) && (g[k+1] == 1)){  // East
+					board.Ax[i][j] = i;
+					board.Ay[i][j] = j+1;
+				}
+				else{
+					if ((g[k] == 1) && (g[k+1] == 0)){	// West
+						board.Ax[i][j] = i;
+						board.Ay[i][j] = j-1;
+					}
+					else{		// South
+							board.Ax[i][j] = i+1;
+							board.Ay[i][j] = j;						
+					}
+				}
+			}
+			k+=2;
+			// Input B (does not check for in-bounds)
+			if ((g[k] == 0) && (g[k+1] == 0)){  // North
+				board.Bx[i][j] = i-1;
+				board.By[i][j] = j;
+			}
+			else{
+				if ((g[k] == 0) && (g[k+1] == 1)){  // East
+					board.Bx[i][j] = i;
+					board.By[i][j] = j+1;
+				}
+				else{
+					if ((g[k] == 1) && (g[k+1] == 0)){	// West
+						board.Bx[i][j] = i;
+						board.By[i][j] = j-1;
+					}
+					else{		// South
+							board.Bx[i][j] = i+1;
+							board.By[i][j] = j;						
+					}
+				}
+			}
+			k+=2;
+			// Gate
+			board.gates[i][j] = g[k];
+			k++;
+		}
+	}
+
+	// Initialize state
+	board.ZeroBlockState();
+	//board.RandomizeBlockState();
+
+	cout << board << endl;
+
+	// Simulate X times
+	cout << board.states << endl;
+	// Write to file
+	ofstream fout("states.dat");
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 1; j <= N; j++)
+		{
+			fout << board.states[i][j] << " ";
+		}
+	}
+	fout << endl;
+	for (int i = 0; i < 8; i++){
+		board.Step();
+		cout << ">" << i+1 << "\n" << board.states << endl;
+		for (int i = 1; i <= N; i++)
+		{
+			for (int j = 1; j <= N; j++)
+			{
+				fout << board.states[i][j] << " ";
+			}
+		}
+		fout << endl;
+	}
+	fout.close();
+
+
+	return 0.0;
+}
+
+// ------------------------------------
+// N Behavior
+// ------------------------------------
+double RandomBehavior()
+{
+	// Create the board
+	FPGA board(N);
+
+	TVector<int> g;
+	g.SetBounds(1,VectSize);
+	for (int i = 0; i < VectSize; i++){
+		g[i] = ProbabilisticChoice(0.5);
+	}
+	cout << g << endl;
+
+	// Set gates and inputs from genotype to board
+	int k = 1;
+	for (int i = 1; i <= N; i++){
+		for (int j = 1; j <= N; j++){
+			// Input A (does not check for in-bounds)
+			if ((g[k] == 0) && (g[k+1] == 0)){  // North
+				board.Ax[i][j] = i-1;
+				board.Ay[i][j] = j;
+			}
+			else{
+				if ((g[k] == 0) && (g[k+1] == 1)){  // East
+					board.Ax[i][j] = i;
+					board.Ay[i][j] = j+1;
+				}
+				else{
+					if ((g[k] == 1) && (g[k+1] == 0)){	// West
+						board.Ax[i][j] = i;
+						board.Ay[i][j] = j-1;
+					}
+					else{		// South
+							board.Ax[i][j] = i+1;
+							board.Ay[i][j] = j;						
+					}
+				}
+			}
+			k+=2;
+			// Input B (does not check for in-bounds)
+			if ((g[k] == 0) && (g[k+1] == 0)){  // North
+				board.Bx[i][j] = i-1;
+				board.By[i][j] = j;
+			}
+			else{
+				if ((g[k] == 0) && (g[k+1] == 1)){  // East
+					board.Bx[i][j] = i;
+					board.By[i][j] = j+1;
+				}
+				else{
+					if ((g[k] == 1) && (g[k+1] == 0)){	// West
+						board.Bx[i][j] = i;
+						board.By[i][j] = j-1;
+					}
+					else{		// South
+							board.Bx[i][j] = i+1;
+							board.By[i][j] = j;						
+					}
+				}
+			}
+			k+=2;
+			// Gate
+			board.gates[i][j] = g[k];
+			k++;
+		}
+	}
+
+	// Initialize state
+	//board.ZeroBlockState();
+	board.RandomizeBlockState();
+
+	cout << board << endl;
+
+	// Simulate X times
+	cout << board.states << endl;
+	// Write to file
+	ofstream fout("states.dat");
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 1; j <= N; j++)
+		{
+			fout << board.states[i][j] << " ";
+		}
+	}
+	fout << endl;
+	for (int i = 0; i < 8; i++){
+		board.Step();
+		cout << ">" << i+1 << "\n" << board.states << endl;
+		for (int i = 1; i <= N; i++)
+		{
+			for (int j = 1; j <= N; j++)
+			{
+				fout << board.states[i][j] << " ";
+			}
+		}
+		fout << endl;
+	}
+	fout.close();
+
+	return 0.0;
+}
+
+// ------------------------------------
+// Fitness function
+// ------------------------------------
+double RecordBehavior(TVector<int> &g)
+{
+	// Create the board
+	FPGA board(N);
+
+	// Set gates and inputs from genotype to board
+	int k = 1;
+	for (int i = 1; i <= N; i++){
+		for (int j = 1; j <= N; j++){
+			// Input A (does not check for in-bounds)
+			if ((g[k] == 0) && (g[k+1] == 0)){  // North
+				board.Ax[i][j] = i-1;
+				board.Ay[i][j] = j;
+			}
+			else{
+				if ((g[k] == 0) && (g[k+1] == 1)){  // East
+					board.Ax[i][j] = i;
+					board.Ay[i][j] = j+1;
+				}
+				else{
+					if ((g[k] == 1) && (g[k+1] == 0)){	// West
+						board.Ax[i][j] = i;
+						board.Ay[i][j] = j-1;
+					}
+					else{		// South
+							board.Ax[i][j] = i+1;
+							board.Ay[i][j] = j;						
+					}
+				}
+			}
+			k+=2;
+			// Input B (does not check for in-bounds)
+			if ((g[k] == 0) && (g[k+1] == 0)){  // North
+				board.Bx[i][j] = i-1;
+				board.By[i][j] = j;
+			}
+			else{
+				if ((g[k] == 0) && (g[k+1] == 1)){  // East
+					board.Bx[i][j] = i;
+					board.By[i][j] = j+1;
+				}
+				else{
+					if ((g[k] == 1) && (g[k+1] == 0)){	// West
+						board.Bx[i][j] = i;
+						board.By[i][j] = j-1;
+					}
+					else{		// South
+							board.Bx[i][j] = i+1;
+							board.By[i][j] = j;						
+					}
+				}
+			}
+			k+=2;
+			// Gate
+			board.gates[i][j] = g[k];
+			k++;
+		}
+	}
+
+	// Initialize state
+	board.ZeroBlockState();
+
+	cout << board.gates << endl;
+	
+	// Count the number of times blocks change
+	double total = 0.0;
+	TMatrix<int> previousboardstate;
+	previousboardstate.SetBounds(1,N,1,N);
+	previousboardstate.FillContents(0.0);
+	// Save latest state
+	for (int i = 1; i <= N; i++){
+		for (int j = 1; j <= N; j++){
+			previousboardstate[i][j] = board.states[i][j];
+		}
+	}
+
+	// Write to file
+	ofstream fout("states.dat");
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 1; j <= N; j++)
+		{
+			fout << board.states[i][j] << " ";
+		}
+	}
+	fout << endl;
+
+	// Simulate for DUR steps
+	for (int t = 1; t <= DUR; t++){
+		board.Step();
+
+		for (int i = 1; i <= N; i++)
+		{
+			for (int j = 1; j <= N; j++)
+			{
+				fout << board.states[i][j] << " ";
+			}
+		}
+		fout << endl;
+
+		double portion = 0.0;
+		// Analyze difference and count
+		for (int i = 1; i <= N; i++){
+			for (int j = 1; j <= N; j++){
+				if (board.states[i][j] != previousboardstate[i][j]){
+					portion += 1.0;
+				}
+			}
+		}
+		total += portion/(N*N);
+		// Save latest state
+		for (int i = 1; i <= N; i++){
+			for (int j = 1; j <= N; j++){
+				previousboardstate[i][j] = board.states[i][j];
+			}
+		}
+	}
+	fout.close();
+	cout << total/DUR << endl;
+	return total/DUR;
+}
+
+// ------------------------------------
+// Display functions
+// ------------------------------------
+void EvolutionaryRunDisplay(int Generation, double BestPerf, double AvgPerf, double PerfVar)
+{
+	cout << Generation << " " << BestPerf << " " << AvgPerf << " " << PerfVar << endl;
+}
+
+void ResultsDisplay(BSearch &s)
+{
+	TVector<int> bestVector;
+	ofstream BestIndividualFile;
+
+	// Save the genotype of the best individual
+	bestVector = s.BestIndividual();
+	BestIndividualFile.open("best.gen.dat");
+	BestIndividualFile << bestVector << endl;
+	BestIndividualFile.close();
+}
+
+// ------------------------------------
+// The main program
+// ------------------------------------
+int main (int argc, const char* argv[]) 
+{
+	long randomseed = static_cast<long>(time(NULL));
+	if (argc == 2)
+		randomseed += atoi(argv[1]);
+
+	BSearch s(VectSize);
+	
+	#ifdef PRINTOFILE
+	ofstream file;
+	file.open("evol.dat");
+	cout.rdbuf(file.rdbuf());
+	// save the seed to a file
+	ofstream seedfile;
+	seedfile.open ("seed.dat");
+	seedfile << randomseed << endl;
+	seedfile.close();
+	#endif
+	
+	// Configure the search
+	s.SetRandomSeed(randomseed);
+	s.SetSearchResultsDisplayFunction(ResultsDisplay);
+	s.SetPopulationStatisticsDisplayFunction(EvolutionaryRunDisplay);
+	s.SetSelectionMode(RANK_BASED);
+	s.SetReproductionMode(GENETIC_ALGORITHM);
+	s.SetPopulationSize(POPSIZE);
+	s.SetMaxGenerations(GENS);
+	s.SetCrossoverProbability(CROSSPROB);
+	s.SetCrossoverMode(UNIFORM);
+	s.SetMutationVariance(MUTVAR);
+	s.SetMaxExpectedOffspring(EXPECTED);
+	s.SetElitistFraction(ELITISM);
+	s.SetSearchConstraint(1);
+	
+	s.SetEvaluationFunction(FitnessFunction); 
+	s.ExecuteSearch();
+	
+	RecordBehavior(s.BestIndividual());
+	return 0;
+}
